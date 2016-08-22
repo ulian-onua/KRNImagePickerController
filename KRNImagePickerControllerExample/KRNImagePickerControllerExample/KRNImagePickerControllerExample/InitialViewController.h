500048a0129d67443ce1378853fdc9d490ad08a6
ulian-onua <ulian.onua@gmail.com>
2016-08-22 15:02:57 +0300

    Add Initial View Controller for memory leaks testing

---
+++ KRNImagePickerControllerExample/KRNImagePickerControllerExample/KRNImagePickerControllerExample/InitialViewController.h
@@ -0,0 +1,13 @@
+//
+//  InitialViewController.h
+//  KRNImagePickerControllerExample
+//
+//  Created by ulian_onua on 22.08.16.
+//  Copyright © 2016 ulian_onua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialViewController : UIViewController
+
+@end
